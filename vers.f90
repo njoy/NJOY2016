32234fae350b018d4f23b386aba06dc0ea68c8a4
Austin McCartney <amccartney@lanl.gov>
2016-11-24 15:06:57 -0700

    src dir

--- vers.f90
+++
@@ -1,9 +0,0 @@
-module version
-   ! Gives the NJOY version identifier and the corresponding date.
-   ! These values are updated during the NJOY revision-control process.
-   implicit none
-   private
-   character(8),public::vers='2012.0'
-   character(8),public::vday='21Dec12'
-end module version
-
