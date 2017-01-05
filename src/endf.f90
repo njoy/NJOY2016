73a7febd4207be81388c8cad5ecce79fa67ed2e9
Austin McCartney <amccartney@lanl.gov>
2017-01-05 15:53:31 -0700

    line endings

--- src/endf.f90
+++ src/endf.f90
@@ -18,7 +18,6 @@ module endf
    !--Public variables
    integer,public::npage=306
    integer,public::iverf
-   integer,public::iverf
    real(kr),public::c1h,c2h
    integer,public::l1h,l2h,n1h,n2h,math,mfh,mth,nsh,nsp,nsc
    real(kr),public::thr6
--- src/mainio.f90
+++ src/mainio.f90
@@ -7,10 +7,6 @@ module mainio
    private
    save
 
-   integer,public::nsysi=INPUT_UNIT     !read from standard input
-   integer,public::nsyso=ERROR_UNIT     !write to an output file
-   integer,public::nsyse=OUTPUT_UNIT    !write to the terminal
-
    integer,public::nsysi=INPUT_UNIT   !standard input,  legacy value=5
    integer,public::nsyso=ERROR_UNIT   !output file,     legacy value=7
    integer,public::nsyse=OUTPUT_UNIT  !terminal output, legacy value=6
