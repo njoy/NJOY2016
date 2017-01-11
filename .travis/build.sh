dc63d31d91ae48923ef037f3a4d7845ced7aba89
Austin McCartney <amccartney@lanl.gov>
2017-01-11 15:45:08 -0700

    travis integration attempt

---
+++ .travis/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -x
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 90 \
+	 --slave /usr/bin/g++ g++ /usr/bin/g++-6 \
+	 --slave /usr/bin/gfortran gfortran /usr/bin/gfortran-6 \
+	 --slave /usr/bin/gcov gcov /usr/bin/gcov-6 \
+	 --slave /usr/bin/gcc-ar ar /usr/bin/gcc-ar-6 \
+	 --slave /usr/bin/gcc-nm nm /usr/bin/gcc-nm-6 \
+	 --slave /usr/bin/gcc-ranlib ranlib /usr/bin/gcc-ranlib-6
+    sudo update-alternatives --config gcc
+    export CUSTOM=('-D CMAKE_AR=/usr/bin/gcc-ar' '-D CMAKE_NM=/usr/bin/gcc-nm' '-D CMAKE_RANLIB=/usr/bin/gcc-ranlib')
+  fi;
+fi
+
+mkdir build
+cd build
+cmake ${CUSTOM[@]}\
+      -D build_type=$build_type \
+      -D static_libraries=$static_libraries \
+      -D appended_flags="$appended_flags" ..
+make -j2
+export COMPILATION_FAILURE=$?
+if [ $COMPILATION_FAILURE -ne 0 ];
+then
+  exit 1
+fi
+
+ctest --output-on-failure -j2
+export TEST_FAILURE=$?
+if [ $TEST_FAILURE -ne 0 ];
+then
+    exit 1
+fi
+if [ "$build_type" = "coverage" ]
+then
+  pip install --user cpp-coveralls
+  echo "loading coverage information"
+  coveralls  --exclude-pattern "/usr/include/.*|.*/CMakeFiles/.*|.*subprojects.*|.*dependencies.*|.*test\.cpp" --root ".." --build-root "." --gcov-options '\-lp'
+fi
