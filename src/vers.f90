e73380a9b40978f4e09a8690049f397adc0b6780
Wim Haeck <whaeck@gmail.com>
2017-12-15 09:30:35 -0700

    Set NJOY2016 version number to 21

--- src/vers.f90
+++ src/vers.f90
@@ -3,7 +3,7 @@ module version
    ! These values are updated during the NJOY revision-control process.
    implicit none
    private
-   character(8),public::vers='2016.20'
-   character(8),public::vday='30Jul17'
+   character(8),public::vers='2016.21'
+   character(8),public::vday='15Dec17'
 end module version
 
