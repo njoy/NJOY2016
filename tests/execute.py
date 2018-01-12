14b2967174aa63197b10e1713c35cfa3b1312174
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-01-12 15:18:44 -0700

    Making a fuzzy diff of the numbers in any output file.

--- tests/execute.py
+++ tests/execute.py
@@ -1,32 +1,132 @@
 #!/usr/bin/env python
 
-import difflib
 import filecmp
-import fileinput
 import glob
 import os
 import re
 import subprocess
 
-retained_tapes = set( glob.glob('tape*') )
+
+floatPattern = re.compile("""(
+                                (
+                                    [-+]?				# sign (optional)
+                                    \d+					# digit(s)
+                                )
+                                (
+                                    \.              # period
+                                    \d+  		    # digit(s)
+                                )?                  # period+digits  (optional)
+                                (
+                                    ([eE])?			# designation (optional)
+                                    [-+]			# sign (required)
+                                    \d+				# digit(s) (required)
+                                )? 					# exponent (optional)
+                            )
+                          """, re.VERBOSE)
+
+
+def identicalLines(refLines, trialLines, diffFile,
+                   relativeError=1E-8, absoluteError=1E-8):
+    """
+    Look at two lists of strings (presumably lines from two files) and compare
+    them; that is, determine if they are the same within the given tolerance.
+    Returns True if everything is the same, returns False if anythin differs.
+    """
+    if len(refLines) != len(trialLines):
+        print("Reference file and trial file have different number of lines!")
+        return False
+
+    equivalent = True
+    for n, (ref, trial) in enumerate(zip(refLines, trialLines)):
+        if ref != trial:
+
+            # Look for numbers
+            refFloats = floatPattern.findall(ref)
+            if refFloats:
+                floatEquivalence = True
+                trialFloats = floatPattern.findall(trial)
+
+                if len(refFloats) != len(trialFloats):
+                    print("Found wrong number of floats on line: {}".format(n))
+                    floatEquivalence = False
+                else:
+                    # Look at all the floats on the lines
+                    for rF, tF in zip(refFloats, trialFloats):
+                        equal = fuzzyDiff(makeFloat(rF), makeFloat(tF),
+                                          relativeError, absoluteError)
+                        if not equal:
+                            print("{} and {} are not equal".format(
+                                rF[0], tF[0]))
+                            floatEquivalence = False
+
+                if not floatEquivalence:
+                    equivalent = False
+                    writeDiff(diffFile, ref, trial, n)
+
+            # Lines contain only text (no numbers)
+            else:
+                equivalent = False
+                writeDiff(diffFile, ref, trial, n)
+
+        # Lines are the the same
+        else:
+            continue
+
+    return equivalent
+
+
+def makeFloat(D):
+    """
+    """
+    try:
+        return float(D[0])
+    except (TypeError, ValueError):
+        if not D[-1]:
+            d = "{}{}E{}".format(D[1], D[2], D[3])
+            f = float(d)
+            return f
+
+
+def writeDiff(diff_file, refLine, trialLine, lineNumber):
+    """
+    Write the differences to the diff file.
+    """
+    diff_file.write("***************\n")
+    diff_file.write("*** {} ***\n".format(lineNumber+1))
+    diff_file.write("!{}".format(refLine))
+    diff_file.write("--- {} ---\n".format(lineNumber+1))
+    diff_file.write("!{}".format(trialLine))
+
+
+def fuzzyDiff(a, b, relativeError=1E-6, absoluteError=1E-8):
+    """
+    fuzzDiff will compare to numbers (a and b) to see if they are identical
+    within the given tolerance. Returns bool.
+    """
+    diff = a - b
+    delta = max(abs(a), abs(b))*relativeError + absoluteError
+    return (diff <= abs(delta))
+
+
+retained_tapes = set(glob.glob('tape*'))
 reference_tapes = glob.glob('referenceTape*')
 
 with open('input', 'r') as i, \
      open('output', 'w') as o, \
-     open('error', 'w') as e :
+     open('error', 'w') as e:
     njoy = os.path.join('..', '..', 'njoy')
     child = subprocess.Popen(njoy, stdin=i, stdout=o, stderr=e)
     child.communicate()
-    if ( child.poll() ):
+    if (child.poll()):
         print("Error enountered while running NJOY!")
-        exit( child.poll() )
+        exit(child.poll())
 
     for reference_tape in reference_tapes:
         trial_tape = 'tape' + reference_tape[-2:]
         if not filecmp.cmp(reference_tape, trial_tape):
             with open(reference_tape, 'r') as reference_file, \
                  open(trial_tape, 'r') as trial_file, \
-                 open(trial_tape + '_diff', 'w') as diff_file :
+                 open(trial_tape + '_diff', 'w') as diff_file:
                 should_exit = False
                 reference_lines = reference_file.readlines()
                 trial_lines = trial_file.readlines()
@@ -37,22 +137,22 @@ with open('input', 'r') as i, \
                                       r'XX/XX/XX', line)
                                for line in trial_lines]
 
-                if reference_lines != trial_lines:
-                    should_exit = True
-                    for line in difflib.context_diff(reference_lines,
-                                                     trial_lines,
-                                                     fromfile=reference_tape,
-                                                     tofile=trial_tape, n=0):
-                        diff_file.write(line)
-                      
-            if should_exit: exit(99)
+                diff_file.write("*** {} ***\n".format(reference_tape))
+                diff_file.write("--- {} ---\n".format(trial_tape))
 
-    removed_tapes = list( set( glob.glob('tape*') ) - retained_tapes )
+                identical = identicalLines(
+                    reference_lines, trial_lines, diff_file, 1E-9, 1E-10)
+                should_exit = not identical
+
+            if should_exit:
+                exit(99)
+
+    removed_tapes = list(set(glob.glob('tape*')) - retained_tapes)
     for tape in removed_tapes:
-        os.remove( tape )
-        
+        os.remove(tape)
+
     for diff in glob.glob('*_diff'):
-        os.remove( diff )
-        
+        os.remove(diff)
+
 os.remove('output')
 os.remove('error')
