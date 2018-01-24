e3f73bec2b9ec3fe7c6a1ad0dc5110cd2e73e624
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-01-23 22:30:42 -0500

    Capturing results for AWS

--- .AWS/gnu_build.sh
+++ .AWS/gnu_build.sh
@@ -7,18 +7,23 @@ export CXX=$(which g++)
 export CORES=$(grep -c ^processor /proc/cpuinfo)
 echo "Number cores: ${CORES}"
 
-build_types=(Debug Release)
-static_types=(TRUE FALSE)
+# build_types=(Debug Release)
+# static_types=(TRUE FALSE)
+build_types=(Debug)
+static_types=(TRUE)
 
 pwd=$PWD
 
+results=()
 for bt in "${build_types[@]}"; do
   for st in "${static_types[@]}"; do
     dir_name="gnu_${bt}_${st}"
     mkdir ${dir_name}
     ./.AWS/build.sh ${dir_name} ${bt} ${st}
     ./.AWS/test.sh ${dir_name} ${bt} ${st}
+    results+=(${dir_name}, $?)
     cd ${pwd}
   done
 done
 
+echo ${results}
--- .AWS/test.sh
+++ .AWS/test.sh
@@ -6,7 +6,4 @@ echo `pwd`
 ctest --output-on-failure -j${CORES}
 
 export TEST_FAILURE=$?
-if [ $TEST_FAILURE -ne 0 ];
-then
-    exit 1
-fi
+return $?
