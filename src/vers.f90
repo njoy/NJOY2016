0141190fb07021a26106964921b98d0047db2bae
Wim Haeck <wim@lanl.gov>
2018-03-27 08:05:12 -0600

    Update vers.f90
    Forgot to change version and date during last pull request

--- src/vers.f90
+++ src/vers.f90
@@ -3,7 +3,7 @@ module version
    ! These values are updated during the NJOY revision-control process.
    implicit none
    private
-   character(8),public::vers='2016.30'
-   character(8),public::vday='21Mar18'
+   character(8),public::vers='2016.31'
+   character(8),public::vday='27Mar18'
 end module version
 
