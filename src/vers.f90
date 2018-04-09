2b5607c271773ac125f855a629a8c8ce97f8c855
Jose Ignacio Marquez Damian <marquezj@cab.cnea.gov.ar>
2018-04-09 14:57:55 -0300

    Change version

--- src/vers.f90
+++ src/vers.f90
@@ -3,7 +3,7 @@ module version
    ! These values are updated during the NJOY revision-control process.
    implicit none
    private
-   character(8),public::vers='2016.31'
-   character(8),public::vday='27Mar18'
+   character(8),public::vers='2016.33'
+   character(8),public::vday='09Apr18'
 end module version
 
