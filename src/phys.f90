9607755b6b14b454c848e1d48bd7da8768b6b63c
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-09-12 09:26:00 -0600

    Updating amasse=electron mass in amu.

--- src/phys.f90
+++ src/phys.f90
@@ -39,7 +39,7 @@ module physics
    real(kr),parameter,public::amasst=3.01550071632e0_kr   !triton
    real(kr),parameter,public::amassh=3.014932234673e0_kr !hellion (3)
    real(kr),parameter,public::amassa=4.001506179127e0_kr  !alpha
-   real(kr),parameter,public::amasse=5.485799110e-4_kr  !electron
+   real(kr),parameter,public::amasse=5.485799090e-4_kr  !electron
    real(kr),parameter,public::epair=amasse*amu*clight*clight/ev
 end module physics
 
