e2453a0c024940090bce1cb31745180937d82947
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-09-12 09:20:25 -0600

    Updating amassa=alpha mass in amu.

--- src/phys.f90
+++ src/phys.f90
@@ -38,7 +38,7 @@ module physics
    real(kr),parameter,public::amassd=2.013553212745e0_kr !deuteron
    real(kr),parameter,public::amasst=3.01550071632e0_kr   !triton
    real(kr),parameter,public::amassh=3.014932234673e0_kr !hellion (3)
-   real(kr),parameter,public::amassa=4.0015061747e0_kr  !alpha
+   real(kr),parameter,public::amassa=4.001506179127e0_kr  !alpha
    real(kr),parameter,public::amasse=5.485799110e-4_kr  !electron
    real(kr),parameter,public::epair=amasse*amu*clight*clight/ev
 end module physics
