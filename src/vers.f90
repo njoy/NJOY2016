b0c216291d011d05c5e4f27ee34f6fcb55f885ab
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-11-13 15:37:24 -0500

    Updating version and date.

--- src/vers.f90
+++ src/vers.f90
@@ -3,7 +3,7 @@ module version
    ! These values are updated during the NJOY revision-control process.
    implicit none
    private
-   character(8),public::vers='2016.43'
-   character(8),public::vday='20Sep18'
+   character(8),public::vers='2016.45'
+   character(8),public::vday='13Nov18'
 end module version
 
