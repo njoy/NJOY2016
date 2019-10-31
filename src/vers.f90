547a1c339b5f2bd97cde6c4acf4312a889b14fa2
Wim Haeck <whaeck@gmail.com>
2019-10-31 17:03:26 -0600

    Increased version number, solving issue #138 (still test to be added and changes in groupr to be finalised)

--- src/vers.f90
+++ src/vers.f90
@@ -3,7 +3,7 @@ module version
    ! These values are updated during the NJOY revision-control process.
    implicit none
    private
-   character(8),public::vers='2016.51'
-   character(8),public::vday='01Nov19'
+   character(8),public::vers='2016.52'
+   character(8),public::vday='xxNov19'
 end module version
 
