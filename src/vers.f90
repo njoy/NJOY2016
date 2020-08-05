d146efc69ea2b579000c83092b267f2d2c6b59df
Wim Haeck <whaeck@gmail.com>
2020-08-04 19:41:08 -0600

    Changed version and date

--- src/vers.f90
+++ src/vers.f90
@@ -3,6 +3,6 @@ module version
    ! These values are updated during the NJOY revision-control process.
    implicit none
    private
-   character(8),public::vers='2016.59'
-   character(8),public::vday='27Jul20'
+   character(8),public::vers='2016.60'
+   character(8),public::vday='05Aug20'
 end module version
