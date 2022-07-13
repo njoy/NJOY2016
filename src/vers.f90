a9695cfe4c3f049b90551223e3f845cb6e00fa13
gsiemers <gsiemers@lanl.gov>
2022-07-13 09:58:21 -0600

    Changing release notes, version, and test description

--- src/vers.f90
+++ src/vers.f90
@@ -3,6 +3,6 @@ module version
    ! These values are updated during the NJOY revision-control process.
    implicit none
    private
-   character(8),public::vers='2016.67'
-   character(8),public::vday='26May22'
+   character(8),public::vers='2016.68'
+   character(8),public::vday='xxXxx22'
 end module version
