ccdd2e0493a4ef75e0fe3e64dec031816a4a1700
Matthew R. Sternat <matt.sternat@gmail.com>
2023-03-29 16:05:31 -0600

    removed old source location correctly

--- Git-Clone/NJOY2016/src/vers.f90
+++
@@ -1,8 +0,0 @@
-module version
-   ! Gives the NJOY version identifier and the corresponding date.
-   ! These values are updated during the NJOY revision-control process.
-   implicit none
-   private
-   character(8),public::vers='2021.04'
-   character(8),public::vday='22Sept21'
-end module version
