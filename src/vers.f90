cec72ac0f502649aca64fd1e72d5c2d87d04ee06
Wim Haeck <whaeck@gmail.com>
2025-02-03 09:41:59 -0700

    Upping version number

--- src/vers.f90
+++ src/vers.f90
@@ -3,6 +3,6 @@ module version
    ! These values are updated during the NJOY revision-control process.
    implicit none
    private
-   character(8),public::vers='2016.77'
-   character(8),public::vday='xxSep24'
+   character(8),public::vers='2016.78'
+   character(8),public::vday='03Feb25'
 end module version
